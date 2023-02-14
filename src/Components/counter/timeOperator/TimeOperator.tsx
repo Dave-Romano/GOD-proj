112398c76fbd9816de2978ec29f06afc3a02e815
Dave-Romano <dave@online.ua>
2023-02-14 14:56:33 +0200

    ud:14.02.2022

--- src/Components/counter/timeOperator/TimeOperator.tsx
+++ src/Components/counter/timeOperator/TimeOperator.tsx
@@ -7,7 +7,7 @@ const minutes = [1, 2, 5, 10, 15, 30].map((item) => `${item} ${MIN}`);
 const hours = [1, 2, 3, 6, 12, 24].map((item) => `${item} ${HOURS}`);
 
 const timeValues = [...minutes, ...hours];
-
+// change
 const TimeOperator = () => {
   return (
     <>
